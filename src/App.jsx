537e59d75c21ef5d2151711a22115ae06f0a5f32
Junaid Akhter <junaidakhter0803@gmail.com>
2026-08-06 01:22:38 +0530

    Remove unused component imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,12 @@
-import "./App.css"
-import LikeButton from "./LikeButton";
-import Counter from "./Counter";
-import LudoBoard from "./LudoBoard";
+import "./App.css";
 import ToDoList from "./ToDoList";
 
-
 function App() {
-
   return (
     <>
-      {/* <h1>States in React</h1> */}
       <ToDoList />
     </>
   );
 }
 
-export default App
+export default App;
